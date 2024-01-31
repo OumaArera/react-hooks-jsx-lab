bff2948956c8d6d46d9ee90318f590470e85b362
OumaArera <johnouma999@gmail.com>
2024-01-31 14:12:20 +0300

    assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,18 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const customStyle = {
+    color: "firebrick", // Set the color to "firebrick"
+  };
+
+  // Update the content and apply the customStyle to the <h1> element
+  return (
+    <div id="home">
+      <h1 style={customStyle}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
